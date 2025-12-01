b8492dc86915e49cb515346c1227364a157c6b16
QuangTrongLeo <trongphamtg05@gmail.com>
2025-12-01 17:19:28 +0700

    feat: impl LargeSquareCard component

--- src/components/Card/LargeSquareCard/index.js
+++ src/components/Card/LargeSquareCard/index.js
@@ -1,9 +1,43 @@
 import classNames from 'classnames/bind';
 import styles from './style.module.scss';
+import { Link } from 'react-router-dom';
 const st = classNames.bind(styles);
 
-function LargeSquareCard() {
-  return <h1>LargeSquareCard</h1>;
+const formatCurrency = (value) => {
+    if (!value || isNaN(Number(value))) return '0';
+    return new Intl.NumberFormat('vi-VN', {
+        style: 'decimal',
+        minimumFractionDigits: 0,
+    }).format(Number(value));
+};
+
+const roundToThousand = (value) => {
+    return Math.round(value / 1000) * 1000;
+};
+
+function LargeSquareCard({ to = '#', img, name, desc, price, discount }) {
+    const originPrice = discount > 0 ? roundToThousand(price / (1 - discount / 100)) : null;
+    return (
+        <Link to={to} className={st('link-wrapper')}>
+            <div className={st('card', 'card-product')}>
+                <img src={img} className={st('card-img')} alt={name} />
+                <div className={st('card-content')}>
+                    <h2 className={st('card-name')}>{name}</h2>
+                    <p className={st('card-desc')}>{desc}</p>
+                    <div className={st('card-price')}>
+                        <span className={st('price')}>{formatCurrency(price)}đ</span>
+
+                        {discount > 0 && <span className={st('origin-price')}>{formatCurrency(originPrice)}đ</span>}
+                    </div>
+                </div>
+                {discount > 0 && (
+                    <div className={st('discount-badge')}>
+                        <span className={st('discount-text')}>-{discount}%</span>
+                    </div>
+                )}
+            </div>
+        </Link>
+    );
 }
 
 export default LargeSquareCard;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,41 @@
 import classNames from 'classnames/bind';
 import styles from './style.module.scss';
+import { LargeSquareCard } from '../../components';
 const st = classNames.bind(styles);
 
 function Home() {
-  return <h1 className={st('content')}>Home Page</h1>;
+    return (
+        <h1 className={st('content')}>
+            Home Page
+            <LargeSquareCard
+                to="https://xedapthegioi.vn/product/xe-dap-the-thao-galaxy-c100/"
+                img="https://xedapthegioi.vn/wp-content/uploads/2018/03/galaxy-c100-7-768x576.jpg"
+                name="Xe đạp thể thao Galaxy C100"
+                desc="Xe đạp Galaxy"
+                price="3690000"
+                discount="13"
+            />
+            <LargeSquareCard
+                to="https://xedapthegioi.vn/product/xe-dap-the-thao-galaxy-c100/"
+                img="https://xedapthegioi.vn/wp-content/uploads/2018/03/galaxy-c100-7-768x576.jpg"
+                name="Xe đạp thể thao Galaxy C100"
+                price="3690000"
+            />
+            <LargeSquareCard
+                to="https://xedapthegioi.vn/product/xe-dap-the-thao-galaxy-c100/"
+                img="https://xedapthegioi.vn/wp-content/uploads/2018/03/galaxy-c100-7-768x576.jpg"
+                name="Xe đạp thể thao Galaxy C100"
+                price="3690000"
+                discount="13"
+            />
+            <LargeSquareCard
+                to="https://xedapthegioi.vn/product/xe-dap-the-thao-galaxy-c100/"
+                img="https://xedapthegioi.vn/wp-content/uploads/2018/03/galaxy-c100-7-768x576.jpg"
+                name="Xe đạp thể thao Galaxy C100"
+                price="3690000"
+            />
+        </h1>
+    );
 }
 
 export default Home;
