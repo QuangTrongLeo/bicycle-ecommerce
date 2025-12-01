c7388ef710a204c3fcf90de565c6c65b693a9a05
QuangTrongLeo <trongphamtg05@gmail.com>
2025-12-01 18:56:24 +0700

    fix: bug layout of HuyVu

--- src/App.js
+++ src/App.js
@@ -1,45 +1,29 @@
 import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publishRoutes } from './router';
-
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
+import { MainLayout } from './layouts';
 
 function App() {
     return (
         <Router>
-            <div
-                className="app-wrapper"
-                style={{
-                    minHeight: '100vh',
-                    display: 'flex',
-                    flexDirection: 'column',
-                }}>
-                <Navigation />
+            <Routes>
+                {publishRoutes.map((route, idx) => {
+                    const Page = route.page;
+                    const Layout = route.layout !== undefined ? route.layout || Fragment : MainLayout;
 
-                <div style={{ flex: 1 }}>
-                    <Routes>
-                        {publishRoutes.map((route, idx) => {
-                            const Page = route.page;
-                            const Layout = route.layout !== undefined ? route.layout || Fragment : Fragment;
-
-                            return (
-                                <Route
-                                    key={idx}
-                                    path={route.path}
-                                    element={
-                                        <Layout>
-                                            <Page />
-                                        </Layout>
-                                    }
-                                />
-                            );
-                        })}
-                    </Routes>
-                </div>
-
-                <Footer />
-            </div>
+                    return (
+                        <Route
+                            key={idx}
+                            path={route.path}
+                            element={
+                                <Layout>
+                                    <Page />
+                                </Layout>
+                            }
+                        />
+                    );
+                })}
+            </Routes>
         </Router>
     );
 }
--- src/layouts/MainLayout/index.js
+++ src/layouts/MainLayout/index.js
@@ -1,5 +1,5 @@
 import classNames from 'classnames/bind';
-import { Footer, Navigation } from '../../components';
+import { Header, Footer, Navigation } from '../../components';
 import { useSelector, useDispatch } from 'react-redux';
 import { increaseCounter } from '../../redux/action/counterAction';
 
@@ -13,56 +13,9 @@ function MainLayout({ children }) {
 
     return (
         <div className={st('wrapper')}>
-            d ds ds ds
-            <br />
-            ds ds ds
-            <br />
-            ds ds ds
-            <br />
-            ds ds ds
-            <br />
-            ds ds ds
-            <br />
-            ds ds ds
-            <br />
-            ds ds ds
-            <br />
-            ds ds ds
-            <br />
-            ds ds ds
-            <br />v v ds ds ds
-            <br />
-            ds ds ds
-            <br />
-            ds ds ds
-            <br />
-            ds ds ds
-            <br />
-            ds ds ds
-            <br />
-            {/* <div className={st('content-container', 'mx-5')}>
-                <div style={{ padding: '10px', background: '#eef', borderRadius: '6px' }}>
-                    <h3>🔥 Test Redux</h3>
-                    <p>
-                        Giá trị counter hiện tại: <b>{counter}</b>
-                    </p>
-
-                    <button
-                        onClick={() => dispatch(increaseCounter())}
-                        style={{
-                            padding: '8px 12px',
-                            background: '#007bff',
-                            color: '#fff',
-                            border: 'none',
-                            borderRadius: '4px',
-                            cursor: 'pointer',
-                        }}>
-                        Tăng counter
-                    </button>
-                </div> */}
-            {/* Nội dung trang */}
-            {/* {children}
-            </div> */}
+            <Navigation />
+            <Header />
+            <div className="mx-5">{children}</div>
             <Footer />
         </div>
     );
