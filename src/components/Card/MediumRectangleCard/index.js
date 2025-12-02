173a248593ea43034a67c82a3abe6738ddd75c01
QuangTrongLeo <trongphamtg05@gmail.com>
2025-12-02 23:17:59 +0700

    fix: update MediumRectangleCard component

--- src/components/Card/MediumRectangleCard/index.js
+++ src/components/Card/MediumRectangleCard/index.js
@@ -1,42 +1,63 @@
+import { useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './style.module.scss';
 import { Link } from 'react-router-dom';
 const st = classNames.bind(styles);
 
 const formatCurrency = (value) => {
-  if (!value || isNaN(Number(value))) return '0';
-  return new Intl.NumberFormat('vi-VN', {
-    style: 'decimal',
-    minimumFractionDigits: 0,
-  }).format(Number(value));
+    if (!value || isNaN(Number(value))) return '0';
+    return new Intl.NumberFormat('vi-VN', {
+        style: 'decimal',
+        minimumFractionDigits: 0,
+    }).format(Number(value));
 };
 
 const roundToThousand = (value) => {
-  return Math.round(value / 1000) * 1000;
+    return Math.round(value / 1000) * 1000;
 };
 
-function MediumRectangleCard({ to = '#', img, name, price, discount }) {
-  const originPrice = discount > 0 ? roundToThousand(price / (1 - discount / 100)) : null;
-  return (
-    <Link to={to} className={st('link-wrapper')}>
-      <div className={st('card', 'card-product')}>
-        <img src={img} className={st('card-img-top')} alt={name} />
-        <div className={st('card-body')}>
-          <div>Color</div>
-          <h5 className={st('card-name')}>{name}</h5>
-          <div className={st('card-price')}>
-            <span className={st('card-cur-price')}>{formatCurrency(price)}đ</span>
-            {discount > 0 && (
-              <>
-                <span className={st('card-discount-percent')}>-{discount}%</span>
-                <span className={st('card-origin-price')}>{formatCurrency(originPrice)}đ</span>
-              </>
-            )}
-          </div>
-        </div>
-      </div>
-    </Link>
-  );
+function MediumRectangleCard({ to = '#', img, name, price, discount, variants }) {
+    const [currentImg, setCurrentImg] = useState(img);
+    const [selectedColor, setSelectedColor] = useState(variants[0].color_id);
+
+    const originPrice = discount > 0 ? roundToThousand(price / (1 - discount / 100)) : null;
+    const handleButtonClick = (e, variant) => {
+        e.preventDefault();
+        setCurrentImg(variant.img);
+        setSelectedColor(variant.color_id);
+    };
+    return (
+        <Link to={to} className={st('link-wrapper')}>
+            <div className={st('card', 'card-product')}>
+                <img src={currentImg} className={st('card-img-top')} alt={name} />
+                <div className={st('card-body')}>
+                    <div className={st('card-color')}>
+                        {variants.map((variant) => (
+                            <button
+                                key={variant.color_id}
+                                style={{ backgroundColor: variant.hex_code }}
+                                onClick={(e) => handleButtonClick(e, variant)}
+                                className={st('color-btn', {
+                                    selected: selectedColor === variant.color_id,
+                                })}>
+                                {variant.color_name}
+                            </button>
+                        ))}
+                    </div>
+                    <h5 className={st('card-name')}>{name}</h5>
+                    <div className={st('card-price')}>
+                        <span className={st('card-cur-price')}>{formatCurrency(price)}đ</span>
+                        {discount > 0 && (
+                            <>
+                                <span className={st('card-discount-percent')}>-{discount}%</span>
+                                <span className={st('card-origin-price')}>{formatCurrency(originPrice)}đ</span>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </Link>
+    );
 }
 
 export default MediumRectangleCard;
--- src/pages/SubCategory/index.js
+++ src/pages/SubCategory/index.js
@@ -7,87 +7,351 @@ const st = classNames.bind(styles);
 const products = [
     {
         id: 1,
-        img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590894785_f2da60cdf3c759008e4a94cea20275da.jpg',
         name: 'Xe Đạp Fixed Gear Magicbros CX5 Plus',
         price: '3490000',
         discount: '13',
+        variants: [
+            {
+                color_id: 1,
+                color_name: 'Đen',
+                hex_code: 'black',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590878547_778a368ec9d7e33993967797f014b448.jpg',
+                quantity: 50,
+            },
+            {
+                color_id: 2,
+                color_name: 'Trắng',
+                hex_code: '#fff',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590888439_2c315bac3cb2ad805cf3b2c54cac170f.jpg',
+                quantity: 30,
+            },
+            {
+                color_id: 3,
+                color_name: 'Xanh',
+                hex_code: 'blue',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590895965_d1750d13c6fe7c8c7629e038d4931402.jpg',
+                quantity: 0,
+            },
+        ],
     },
     {
         id: 2,
-        img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590894785_f2da60cdf3c759008e4a94cea20275da.jpg',
         name: 'Xe Đạp Fixed Gear Magicbros CX5 Plus',
         price: '3490000',
         discount: '13',
+        variants: [
+            {
+                color_id: 1,
+                color_name: 'Đen',
+                hex_code: 'black',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590878547_778a368ec9d7e33993967797f014b448.jpg',
+                quantity: 50,
+            },
+            {
+                color_id: 2,
+                color_name: 'Trắng',
+                hex_code: '#fff',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590888439_2c315bac3cb2ad805cf3b2c54cac170f.jpg',
+                quantity: 30,
+            },
+            {
+                color_id: 3,
+                color_name: 'Xanh',
+                hex_code: 'blue',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590895965_d1750d13c6fe7c8c7629e038d4931402.jpg',
+                quantity: 0,
+            },
+        ],
     },
     {
         id: 3,
-        img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590894785_f2da60cdf3c759008e4a94cea20275da.jpg',
         name: 'Xe Đạp Fixed Gear Magicbros CX5 Plus',
         price: '3490000',
         discount: '13',
+        variants: [
+            {
+                color_id: 1,
+                color_name: 'Đen',
+                hex_code: 'black',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590878547_778a368ec9d7e33993967797f014b448.jpg',
+                quantity: 50,
+            },
+            {
+                color_id: 2,
+                color_name: 'Trắng',
+                hex_code: '#fff',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590888439_2c315bac3cb2ad805cf3b2c54cac170f.jpg',
+                quantity: 30,
+            },
+            {
+                color_id: 3,
+                color_name: 'Xanh',
+                hex_code: 'blue',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590895965_d1750d13c6fe7c8c7629e038d4931402.jpg',
+                quantity: 0,
+            },
+        ],
     },
     {
         id: 4,
-        img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590894785_f2da60cdf3c759008e4a94cea20275da.jpg',
         name: 'Xe Đạp Fixed Gear Magicbros CX5 Plus',
         price: '3490000',
         discount: '13',
+        variants: [
+            {
+                color_id: 1,
+                color_name: 'Đen',
+                hex_code: 'black',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590878547_778a368ec9d7e33993967797f014b448.jpg',
+                quantity: 50,
+            },
+            {
+                color_id: 2,
+                color_name: 'Trắng',
+                hex_code: '#fff',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590888439_2c315bac3cb2ad805cf3b2c54cac170f.jpg',
+                quantity: 30,
+            },
+            {
+                color_id: 3,
+                color_name: 'Xanh',
+                hex_code: 'blue',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590895965_d1750d13c6fe7c8c7629e038d4931402.jpg',
+                quantity: 0,
+            },
+        ],
     },
     {
         id: 5,
-        img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590894785_f2da60cdf3c759008e4a94cea20275da.jpg',
         name: 'Xe Đạp Fixed Gear Magicbros CX5 Plus',
         price: '3490000',
         discount: '13',
+        variants: [
+            {
+                color_id: 1,
+                color_name: 'Đen',
+                hex_code: 'black',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590878547_778a368ec9d7e33993967797f014b448.jpg',
+                quantity: 50,
+            },
+            {
+                color_id: 2,
+                color_name: 'Trắng',
+                hex_code: '#fff',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590888439_2c315bac3cb2ad805cf3b2c54cac170f.jpg',
+                quantity: 30,
+            },
+            {
+                color_id: 3,
+                color_name: 'Xanh',
+                hex_code: 'blue',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590895965_d1750d13c6fe7c8c7629e038d4931402.jpg',
+                quantity: 0,
+            },
+        ],
     },
     {
         id: 6,
-        img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590894785_f2da60cdf3c759008e4a94cea20275da.jpg',
         name: 'Xe Đạp Fixed Gear Magicbros CX5 Plus',
         price: '3490000',
         discount: '13',
+        variants: [
+            {
+                color_id: 1,
+                color_name: 'Đen',
+                hex_code: 'black',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590878547_778a368ec9d7e33993967797f014b448.jpg',
+                quantity: 50,
+            },
+            {
+                color_id: 2,
+                color_name: 'Trắng',
+                hex_code: '#fff',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590888439_2c315bac3cb2ad805cf3b2c54cac170f.jpg',
+                quantity: 30,
+            },
+            {
+                color_id: 3,
+                color_name: 'Xanh',
+                hex_code: 'blue',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590895965_d1750d13c6fe7c8c7629e038d4931402.jpg',
+                quantity: 0,
+            },
+        ],
     },
     {
         id: 7,
-        img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590894785_f2da60cdf3c759008e4a94cea20275da.jpg',
         name: 'Xe Đạp Fixed Gear Magicbros CX5 Plus',
         price: '3490000',
         discount: '13',
+        variants: [
+            {
+                color_id: 1,
+                color_name: 'Đen',
+                hex_code: 'black',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590878547_778a368ec9d7e33993967797f014b448.jpg',
+                quantity: 50,
+            },
+            {
+                color_id: 2,
+                color_name: 'Trắng',
+                hex_code: '#fff',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590888439_2c315bac3cb2ad805cf3b2c54cac170f.jpg',
+                quantity: 30,
+            },
+            {
+                color_id: 3,
+                color_name: 'Xanh',
+                hex_code: 'blue',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590895965_d1750d13c6fe7c8c7629e038d4931402.jpg',
+                quantity: 0,
+            },
+        ],
     },
     {
         id: 8,
-        img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590894785_f2da60cdf3c759008e4a94cea20275da.jpg',
         name: 'Xe Đạp Fixed Gear Magicbros CX5 Plus',
         price: '3490000',
         discount: '13',
+        variants: [
+            {
+                color_id: 1,
+                color_name: 'Đen',
+                hex_code: 'black',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590878547_778a368ec9d7e33993967797f014b448.jpg',
+                quantity: 50,
+            },
+            {
+                color_id: 2,
+                color_name: 'Trắng',
+                hex_code: '#fff',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590888439_2c315bac3cb2ad805cf3b2c54cac170f.jpg',
+                quantity: 30,
+            },
+            {
+                color_id: 3,
+                color_name: 'Xanh',
+                hex_code: 'blue',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590895965_d1750d13c6fe7c8c7629e038d4931402.jpg',
+                quantity: 0,
+            },
+        ],
     },
     {
         id: 9,
-        img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590894785_f2da60cdf3c759008e4a94cea20275da.jpg',
         name: 'Xe Đạp Fixed Gear Magicbros CX5 Plus',
         price: '3490000',
         discount: '13',
+        variants: [
+            {
+                color_id: 1,
+                color_name: 'Đen',
+                hex_code: 'black',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590878547_778a368ec9d7e33993967797f014b448.jpg',
+                quantity: 50,
+            },
+            {
+                color_id: 2,
+                color_name: 'Trắng',
+                hex_code: '#fff',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590888439_2c315bac3cb2ad805cf3b2c54cac170f.jpg',
+                quantity: 30,
+            },
+            {
+                color_id: 3,
+                color_name: 'Xanh',
+                hex_code: 'blue',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590895965_d1750d13c6fe7c8c7629e038d4931402.jpg',
+                quantity: 0,
+            },
+        ],
     },
     {
         id: 10,
-        img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590894785_f2da60cdf3c759008e4a94cea20275da.jpg',
         name: 'Xe Đạp Fixed Gear Magicbros CX5 Plus',
         price: '3490000',
         discount: '13',
+        variants: [
+            {
+                color_id: 1,
+                color_name: 'Đen',
+                hex_code: 'black',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590878547_778a368ec9d7e33993967797f014b448.jpg',
+                quantity: 50,
+            },
+            {
+                color_id: 2,
+                color_name: 'Trắng',
+                hex_code: '#fff',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590888439_2c315bac3cb2ad805cf3b2c54cac170f.jpg',
+                quantity: 30,
+            },
+            {
+                color_id: 3,
+                color_name: 'Xanh',
+                hex_code: 'blue',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590895965_d1750d13c6fe7c8c7629e038d4931402.jpg',
+                quantity: 0,
+            },
+        ],
     },
     {
         id: 11,
-        img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590894785_f2da60cdf3c759008e4a94cea20275da.jpg',
         name: 'Xe Đạp Fixed Gear Magicbros CX5 Plus',
         price: '3490000',
         discount: '13',
+        variants: [
+            {
+                color_id: 1,
+                color_name: 'Đen',
+                hex_code: 'black',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590878547_778a368ec9d7e33993967797f014b448.jpg',
+                quantity: 50,
+            },
+            {
+                color_id: 2,
+                color_name: 'Trắng',
+                hex_code: '#fff',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590888439_2c315bac3cb2ad805cf3b2c54cac170f.jpg',
+                quantity: 30,
+            },
+            {
+                color_id: 3,
+                color_name: 'Xanh',
+                hex_code: 'blue',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590895965_d1750d13c6fe7c8c7629e038d4931402.jpg',
+                quantity: 0,
+            },
+        ],
     },
     {
         id: 12,
-        img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590894785_f2da60cdf3c759008e4a94cea20275da.jpg',
         name: 'Xe Đạp Fixed Gear Magicbros CX5 Plus',
         price: '3490000',
         discount: '13',
+        variants: [
+            {
+                color_id: 1,
+                color_name: 'Đen',
+                hex_code: 'black',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590878547_778a368ec9d7e33993967797f014b448.jpg',
+                quantity: 50,
+            },
+            {
+                color_id: 2,
+                color_name: 'Trắng',
+                hex_code: '#fff',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590888439_2c315bac3cb2ad805cf3b2c54cac170f.jpg',
+                quantity: 30,
+            },
+            {
+                color_id: 3,
+                color_name: 'Xanh',
+                hex_code: 'blue',
+                img: 'https://xedapthegioi.vn/wp-content/uploads/2024/02/z5568590895965_d1750d13c6fe7c8c7629e038d4931402.jpg',
+                quantity: 0,
+            },
+        ],
     },
 ];
 
@@ -103,9 +367,10 @@ function SubCategory() {
                             to={`${configs.routes.detail}/${product.name}`}
                             name={product.name}
                             desc={product.desc}
-                            img={product.img}
+                            img={product.variants[0].img}
                             price={product.price}
                             discount={product.discount}
+                            variants={product.variants}
                         />
                     </div>
                 ))}
